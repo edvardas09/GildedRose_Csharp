adfbb5eb711203a1459ae0cce71d226cbc8a2819
edvardas09 <edvardassneid@gmail.com>
2020-02-21 17:20:25 +0200

    initial commit

---
+++ GildedRose.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+
+namespace csharp
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {   
+            for (var i = 0; i < Items.Count; i++)
+            {
+                //check if item name starts with "sulfuras"
+                if (!Items[i].Name.StartsWith("Sulfuras")) //if item is not sulfuras, do calculations
+                {
+                    //if item is not sulfuras, decrease SellIn value
+                    Items[i].SellIn -= 1;
+
+                    if (Items[i].SellIn < 0 && Items[i].Quality > -1) //if item's sell by date has passed and quality is above -1
+                    {
+                        //check if item's name starts with "Aged Brie"
+                        if (Items[i].Name == "Aged Brie" && Items[i].Quality < 50)
+                        {
+                            Items[i].Quality += 2;
+                        }
+
+                        //check if item's name starts with "Backstage passes"
+                        else if (Items[i].Name.StartsWith("Backstage passes"))
+                        {
+                            Items[i].Quality = 0;
+                        }
+
+                        //check if item's name starts with "Conjured"
+                        else if (Items[i].Name.StartsWith("Conjured"))
+                        {
+                            Items[i].Quality -= 4;
+                        }
+                        
+                        //for remaining items'
+                        else if (Items[i].Name != "Aged Brie")
+                        {
+                            Items[i].Quality -= 2;
+                        }
+                    }
+
+                    //if item quality between 0 and 50 and sell by date has not passed, change quality
+                    else if (Items[i].Quality > -1 && Items[i].Quality < 50)
+                    {
+                        //check if item's name starts with "Aged Brie"
+                        if (Items[i].Name == "Aged Brie")
+                        {
+                            Items[i].Quality += 1;
+                        }
+
+                        //check if item's name starts with "Backstage passes"
+                        else if (Items[i].Name.StartsWith("Backstage passes"))
+                        {
+                            Items[i].Quality += 1; //always add 1 to quality
+
+                            if (Items[i].SellIn < 10)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    if (Items[i].SellIn < 5)
+                                    {
+                                        Items[i].Quality += 2; //if item's sell by date is less then 5, add 2 more to quality
+                                    }
+                                    else
+                                        Items[i].Quality += 1; //if item's sell by date is less then 10, but more then 5, add 1 more to quality
+                                }
+                            }
+
+                        }
+
+                        //check if item's name starts with "Conjured"
+                        else if (Items[i].Name.StartsWith("Conjured"))
+                        {
+                            Items[i].Quality -= 2;
+                        }
+
+                        //for other items'
+                        else
+                        {
+                            Items[i].Quality -= 1;
+                        }
+                    }
+
+                    //just in case quality drops below 0
+                    if (Items[i].Quality < 0)
+                        Items[i].Quality = 0;
+                    //just in case quality rises above 50
+                    if (Items[i].Quality > 50)
+                        Items[i].Quality = 50;
+                }            
+            }
+        }
+    }
+}
---
+++ GildedRoseTest.cs
@@ -0,0 +1,108 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    [TestFixture]
+    public class GildedRoseTest
+    {
+        [Test]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 2, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("foo", Items[0].Name);
+            Assert.AreEqual(1, Items[0].SellIn);
+            Assert.AreEqual(9, Items[0].Quality);
+        }
+
+        [Test]
+        public void SimpleItem()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("+5 Dexterity Vest", Items[0].Name);
+            Assert.AreEqual(9, Items[0].SellIn);
+            Assert.AreEqual(19, Items[0].Quality);
+        }
+
+        [Test]
+        public void AgedBrie()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 20 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("Aged Brie", Items[0].Name);
+            Assert.AreEqual(9, Items[0].SellIn);
+            Assert.AreEqual(21, Items[0].Quality);
+        }
+
+        [Test]
+        public void Sulfuras()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("Sulfuras, Hand of Ragnaros", Items[0].Name);
+            Assert.AreEqual(0, Items[0].SellIn);
+            Assert.AreEqual(80, Items[0].Quality);
+        }
+
+        [Test]
+        public void Backstage1()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 12, Quality = 25 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("Backstage passes to a TAFKAL80ETC concert", Items[0].Name);
+            Assert.AreEqual(11, Items[0].SellIn);
+            Assert.AreEqual(26, Items[0].Quality);
+        }
+
+        [Test]
+        public void Backstage2()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 9, Quality = 25 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("Backstage passes to a TAFKAL80ETC concert", Items[0].Name);
+            Assert.AreEqual(8, Items[0].SellIn);
+            Assert.AreEqual(27, Items[0].Quality);
+        }
+
+        [Test]
+        public void Backstage3()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 4, Quality = 25 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("Backstage passes to a TAFKAL80ETC concert", Items[0].Name);
+            Assert.AreEqual(3, Items[0].SellIn);
+            Assert.AreEqual(28, Items[0].Quality);
+        }
+
+        [Test]
+        public void Backstage0()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 25 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("Backstage passes to a TAFKAL80ETC concert", Items[0].Name);
+            Assert.AreEqual(-1, Items[0].SellIn);
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+
+        [Test]
+        public void Conjured()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 10, Quality = 25 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("Conjured Mana Cake", Items[0].Name);
+            Assert.AreEqual(9, Items[0].SellIn);
+            Assert.AreEqual(23, Items[0].Quality);
+        }
+    }
+}
